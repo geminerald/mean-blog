34a23fbd1e098913cb0a360f07b86dddf9c98abc
Zac Warner <45380112+geminerald@users.noreply.github.com>
2021-09-14 21:08:23 +0100

    added error service

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -9,11 +9,11 @@ const router = express.Router();
 
 router.post("", checkAuth, extractFile, PostController.createPost);
 
+router.put("/:id", checkAuth, extractFile, PostController.updatePost);
+
 router.get('', PostController.getAllPosts);
 
 router.get("/:id", PostController.getPost);
-
-router.put("/:id", checkAuth, extractFile, PostController.updatePost);
 
 router.delete("/:id", checkAuth, PostController.deletePost);
 
